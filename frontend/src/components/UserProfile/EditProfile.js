552a1d77ce2f64b87913426795580764cf7fb7c6
KuntrapakamJishnu <kjishnu973@gmail.com>
2024-07-11 12:41:12 +0530

    Update EditProfile.js

--- frontend/src/components/UserProfile/EditProfile.js
+++ frontend/src/components/UserProfile/EditProfile.js
@@ -18,7 +18,7 @@ function UserUpdateForm() {
   useEffect(() => {
    
     const user = localStorage.getItem('user');
-    Axios.get("http://localhost:4000/eventRoute/check-user/" + user)
+    Axios.get("https://event-management-system-f7wg-api.vercel.app/eventRoute/check-user/" + user)
       .then(response => {
         {
           setFormData(
